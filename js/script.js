3c273e1cedf10b6b8badcc982ea2a10dd1d0edd7
Larry Lenox <larrylenx9@gmail.com>
2024-09-11 13:19:34 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ let display = document.getElementById("display");
 function handleClick(obj) {
     let inputNum = obj.innerHTML;
     if (inputNum === "=") {
+        try{
         display.innerHTML = eval(display.innerHTML);
+        
+
+
+        }catch (error) {
+            display.innerHTML = "Invalid expression";
+        }
     } else if (inputNum === "C") {
         display.innerHTML = "0";
     } else {
